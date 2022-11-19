def3a8367d5319cca29d2ef7fd4ca91d9a913228
Pooja Bisht <pooja.bisht@nobious.com>
2022-11-19 09:46:54 +0530

    routing added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCustomerComponent } from './components/add-customer/add-customer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HeaderComponent } from './components/header/header.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
 import { LoginComponent } from './components/login/login.component';
+import { MaterialMasterComponent } from './components/material-master/material-master.component';
+import { PersonalInformationComponent } from './components/personal-information/personal-information.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ServiceJobComponent } from './components/service-job/service-job.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'add-customer', component: AddCustomerComponent},
+  {path: 'inventory', component: InventoryComponent},
+  {path: 'material-master', component: MaterialMasterComponent},
+  {path: 'personal-information', component: PersonalInformationComponent},
+  {path: 'service-job', component: ServiceJobComponent},
 ];
 
 @NgModule({
